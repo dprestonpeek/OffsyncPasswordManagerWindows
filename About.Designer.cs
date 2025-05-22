a3b66f1edfde32d2fa5e827037ff5b5af025ae88
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-05-22 12:39:40 -0400

    Updated About page and icons

--- About.Designer.cs
+++ About.Designer.cs
@@ -34,12 +34,15 @@ namespace OffSyncPasswordManager
             label2 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
             button1 = new System.Windows.Forms.Button();
+            label4 = new System.Windows.Forms.Label();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Font = new System.Drawing.Font("Segoe UI", 12F);
             label1.Location = new System.Drawing.Point(127, 9);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(110, 21);
@@ -53,20 +56,20 @@ namespace OffSyncPasswordManager
             label2.Name = "label2";
             label2.Size = new System.Drawing.Size(117, 15);
             label2.TabIndex = 1;
-            label2.Text = "Version 2025.01.09.00";
+            label2.Text = "Version 2025.05.22.00";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(71, 70);
+            label3.Location = new System.Drawing.Point(77, 57);
             label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(224, 15);
+            label3.Size = new System.Drawing.Size(211, 15);
             label3.TabIndex = 2;
-            label3.Text = "Designed and Created by D. Preston Peek";
+            label3.Text = "Designed and Created by Peek Velocity";
             // 
             // button1
             // 
-            button1.Location = new System.Drawing.Point(146, 105);
+            button1.Location = new System.Drawing.Point(145, 179);
             button1.Name = "button1";
             button1.Size = new System.Drawing.Size(75, 23);
             button1.TabIndex = 4;
@@ -74,12 +77,34 @@ namespace OffSyncPasswordManager
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(77, 216);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(220, 15);
+            label4.TabIndex = 5;
+            label4.Text = "© 2025 Peek Velocity. All rights reserved.";
+            label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (System.Drawing.Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new System.Drawing.Point(33, 75);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(318, 98);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
             // About
             // 
             AcceptButton = button1;
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(363, 140);
+            ClientSize = new System.Drawing.Size(363, 249);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
             Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -87,6 +112,7 @@ namespace OffSyncPasswordManager
             Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
             Name = "About";
             Text = "About";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -97,5 +123,7 @@ namespace OffSyncPasswordManager
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
